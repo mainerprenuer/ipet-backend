b36991b6c2c05fd7ff28873c8ca866f1858b2375
Mousty Mainer <moustymainer@gmail.com>
2024-02-07 18:07:11 +0100

    added app schema models

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: true,
+};
+
+app.get("/", (req, res) => {
+    res.send("Api is working..");
+});
+
+//database conn
+mongoose.set('strictQuery', false)
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            udeNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log('MongoDB database is connected')
+    } catch (error) {
+        console.log('MongoDB database connection failed')
+    }
+}
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+
+app.listen(port, () => {
+    connectDB();
+    console.log("Server is running on port" + port);
+})
